3f21738440ec55c1f82d8825e4d5a499e04a9f57
Gerardo Ramirez <Gerardo Ramirez del Rio>
2024-09-01 17:29:46 -0500

    Progress on scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String helpStatement = """
                 Command Syntax:
                         ./lab1_ref [flags] filename
@@ -8,10 +14,7 @@ public class Main {
                         filename  is the pathname (absolute or relative) to the input file
 
                 Optional flags:
-                        -e       suppress error messages
                         -h       prints this message
-                        -l       Opens log file "./Log" and starts logging.
-                        -v       prints version number
 
                 At most one of the following three flags:
                         -s       prints tokens in token stream
@@ -21,40 +24,41 @@ public class Main {
         String fileName = "";
         int option = 0;
         boolean multipleFlags = false;
-        for (int i = 0; i < args.length; i++){
-            if (args[i].equals("-h")) {
-                System.out.println(helpStatement);
-                return;
-            }
-            else if (args[i].equals("-s")){
-                if (option != 0){
-                    multipleFlags = true;
-                }
-                option = 1;
-            }
-            else if (args[i].equals("-p")){
-                if (option != 0) {
-                    multipleFlags = true;
-                }
-                if (option < 2) {
-                    option = 2;
-                }
-            }
-            else if (args[i].equals("-r")) {
-                if (option != 0) {
-                    multipleFlags = true;
-                }
-                if (option < 3) {
-                    option = 3;
-                }
-            }
-            else {
-                if (fileName.isEmpty()) {
-                    fileName = args[i];
-                } else {
-                    System.out.println("ERROR:  Attempt to open more than one input file.\n");
+        for (String arg : args) {
+            switch (arg) {
+                case "-h" -> {
                     System.out.println(helpStatement);
                     return;
+                }
+                case "-s" -> {
+                    if (option != 0) {
+                        multipleFlags = true;
+                    } else {
+                        option = 1;
+                    }
+                }
+                case "-p" -> {
+                    if (option != 0) {
+                        multipleFlags = true;
+                    }
+                    if (option < 2) {
+                        option = 2;
+                    }
+                }
+                case "-r" -> {
+                    if (option != 0) {
+                        multipleFlags = true;
+                    }
+                    option = 3;
+                }
+                default -> {
+                    if (fileName.isEmpty()) {
+                        fileName = arg;
+                    } else {
+                        System.out.println("ERROR:  Attempt to open more than one input file.\n");
+                        System.out.println(helpStatement);
+                        return;
+                    }
                 }
             }
         }
@@ -66,5 +70,45 @@ public class Main {
         if (option == 0){
             option = 2;
         }
+
+        FileReader fileReader = null;
+
+        try {
+            fileReader = new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: Could not open file " + fileName + " as the input file.");
+            return;
+        }
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        switch (option) {
+            case 1:
+                runSFlag(bufferedReader);
+                break;
+            case 2:
+                runPFlag(bufferedReader);
+                break;
+            case 3:
+                runRFlag(bufferedReader);
+                break;
+            default:
+                System.out.println("ERROR:  Unknown option.");
+        }
+
+    }
+
+    public static void runSFlag(BufferedReader bufferedReader) throws IOException {
+        String currentLine = "";
+        while (bufferedReader.ready()) {
+            currentLine = bufferedReader.readLine();
+            }
+        }
+
+    public static void runPFlag(BufferedReader bufferedReader){
+
+    }
+
+    public static void runRFlag(BufferedReader bufferedReader){
+
     }
 }
